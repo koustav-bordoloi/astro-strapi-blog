9f963d36739cc398ca5cfe2e8af7727ecd6cc10a
Koustav Bordoloi <koustavbordoloi@gmail.com>
2025-08-14 11:46:25 +0530

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://koustav-bordoloi.github.io',
+  site: 'https://koustav-bordoloi.github.io/astro-strapi-blog',
+  base: '/astro-strapi-blog/',
+  output: 'static',
 });
